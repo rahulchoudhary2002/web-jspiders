9c0e39650f4428b4cdad061890208854703e3518
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-09-04 22:49:38 +0530

    var,let,const

--- web/javaScript/Day1/demo.js
+++ web/javaScript/Day1/demo.js
@@ -1,1 +1,14 @@
 console.log('first');
+var c1;
+c1=442;
+console.log(c1);
+let c2;
+c2=4;
+console.log(c2);
+// let c2=33;
+c2=44;
+console.log(c2);
+//const c3;//thsi is not allowed in the js const keyword is used with initilization and in const we cannot reinitilize the values
+const c3=3.14;
+console.log(c3);
+// const c3=33;
