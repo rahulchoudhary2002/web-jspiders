aeecc1ad0b4346a53359bed77af0d38f515852e9
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-09-05 21:43:00 +0530

    var,let, const

--- web/javaScript/Day1/second.js
+++ web/javaScript/Day1/second.js
@@ -1,1 +1,21 @@
-console.log('second');
+// console.log('second');
+
+    // example
+    // debugger;
+    // var a;
+    // let a1=10;
+    // const a2=20;
+    // console.log(a);
+    // console.log(a1);
+    // console.log(a2);
+
+    c=90;
+    console.log(c);
+    var c=60;
+    // b=50;
+    let b;
+    b=90;
+    console.log(b); 
+
+
+
