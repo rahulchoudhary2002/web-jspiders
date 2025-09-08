1508e6cf98b25cdcc87d575580f013f58da4acf5
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-09-08 20:31:52 +0530

    js

---
+++ web/javaScript/input/index.js
@@ -0,0 +1,86 @@
+// console.log('type coversion in js');
+
+// let username =prompt('enter the name');
+// console.log.apply(username);
+
+// let num1=prompt('enter the no.1');
+// console.log(num1);
+
+
+// let num2=prompt('enter the no.2')
+// console.log(num2);
+
+// console.log((number)(num1)+(number)(num2));
+
+
+
+        //this are the falsy values  
+
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(false));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0n));
+// console.log(Boolean(NaN));
+
+
+// truthy  values -> other than this values are truthy values....
+
+//logical operators:->
+    //&& 
+        console.log('this the AND operators');
+console.log(0&&30);
+console.log(20&&10);
+console.log(10&&20);
+console.log(50&&30);
+console.log(50&&35);
+    //|| or operator
+    console.log('this the OR operators');
+console.log(20||10);
+console.log(10||20);
+console.log(50||30);
+console.log(50||35);
+
+// let username=prompt('enter your name');
+
+// let age =username&&prompt('enter the age');
+// console.log(username, age);
+
+// let username=prompt('enter your username');
+
+// let age =username&&prompt('enter the age');
+// console.log(username, age);
+
+
+// let user='abc';
+// let age=23;
+
+// console.log('name is :', user  , 'and the age ', age);
+
+
+// console.log(` name is ${user}  and age is ${age} ` );
+
+//string interpulation
+
+// let num1=parseInt(prompt("enter the number 1"));
+// let num2=parseInt(prompt("enter the number 2"));
+// console.log(  `enter the number1 ${num1} enter the number2 ${num2} and the multiplication ${num1*num2}`);
+
+// let age=parseInt(prompt("enter the age"));
+
+// console.log((age>18)?'i would like to have a glass of water' : 'I would like to have a glass of milk')
+
+
+// let username =prompt('eneter the username');
+
+// console.log('hello ', username || 'guest');
+
+let username =prompt('eneter the age');
+
+let msg=`I would like to have a glass of  ${age>18?'something':'milk'}`
+
+console.log(msg);
+
+
+
